bcdfbd5a2efa2178c37f9dadbef4a1620fbb9a9c
sandhiyaAiml2005 <sandmadhavan2005@gmail.com>
2026-09-10 12:58:08 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Duplicate{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        find(arr);
+    }
+    static void find(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.print(arr[i]+" ");               
+                }
+            }
+        }                    
+        }
+}
+
+    
